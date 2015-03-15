cf563ca1a9580925258a180e6158f667a9fb6380
Xorg <xorgbreaker@gmail.com>
2015-03-15 21:54:53 +0100

    Fix embeded build

--- embed.sh
+++ embed.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/sh
 
 mkdir -p embed
-gdk-pixbuf-csource --raw --name=CPU_X			data/CPU-X.png		> embed/CPU-X.png.h
-gdk-pixbuf-csource --raw --name=NOVENDOR		data/novendor.png	> embed/NoVendor.png.h
-gdk-pixbuf-csource --raw --name=AMD			data/AMD.png		> embed/AMD.png.h
-gdk-pixbuf-csource --raw --name=Centaur			data/Centaur.png	> embed/Centaur.png.h
-gdk-pixbuf-csource --raw --name=Cyrix			data/Cyrix.png		> embed/Cyrix.png.h
-gdk-pixbuf-csource --raw --name=Intel			data/Intel.png		> embed/Intel.png.h
-gdk-pixbuf-csource --raw --name=National_Semiconductor  data/NSC.png		> embed/NSC.png.h
-gdk-pixbuf-csource --raw --name=NexGen			data/NexGen.png		> embed/NexGen.png.h
-gdk-pixbuf-csource --raw --name=Rise			data/Rise.png		> embed/Rise.png.h
-gdk-pixbuf-csource --raw --name=SiS			data/SiS.png		> embed/SiS.png.h
-gdk-pixbuf-csource --raw --name=Transmeta		data/Transmeta.png	> embed/Transmeta.png.h
-gdk-pixbuf-csource --raw --name=UMC			data/UMC.png		> embed/UMC.png.h
+gdk-pixbuf-csource --raw --name=CPU_X			data/pictures/CPU-X.png		> embed/CPU-X.png.h
+gdk-pixbuf-csource --raw --name=NOVENDOR		data/pictures/novendor.png	> embed/NoVendor.png.h
+gdk-pixbuf-csource --raw --name=AMD			data/pictures/AMD.png		> embed/AMD.png.h
+gdk-pixbuf-csource --raw --name=Centaur			data/pictures/Centaur.png	> embed/Centaur.png.h
+gdk-pixbuf-csource --raw --name=Cyrix			data/pictures/Cyrix.png		> embed/Cyrix.png.h
+gdk-pixbuf-csource --raw --name=Intel			data/pictures/Intel.png		> embed/Intel.png.h
+gdk-pixbuf-csource --raw --name=National_Semiconductor  data/pictures/NSC.png		> embed/NSC.png.h
+gdk-pixbuf-csource --raw --name=NexGen			data/pictures/NexGen.png	> embed/NexGen.png.h
+gdk-pixbuf-csource --raw --name=Rise			data/pictures/Rise.png		> embed/Rise.png.h
+gdk-pixbuf-csource --raw --name=SiS			data/pictures/SiS.png		> embed/SiS.png.h
+gdk-pixbuf-csource --raw --name=Transmeta		data/pictures/Transmeta.png	> embed/Transmeta.png.h
+gdk-pixbuf-csource --raw --name=UMC			data/pictures/UMC.png		> embed/UMC.png.h
 
 echo -e "#ifndef CPUX_GLADE_H\n#define CPUX_GLADE_H\n\nstatic const char *cpux_glade =" > embed/cpu-x.ui.h
 sed -e 's/\\/\\\\/g;s/"/\\"/g;s/ / /g;s/^/"/;s/$/\\n"/' 'data/cpux-gtk-3.8.ui' >> embed/cpu-x.ui.h
