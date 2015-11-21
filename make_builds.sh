713bd1aae4a9b548514d0e9fb136793f5f2457bd
Xorg <xorgbreaker@gmail.com>
2015-11-21 18:51:13 +0100

    Update make_builds.sh script

--- make_builds.sh
+++ make_builds.sh
@@ -14,31 +14,43 @@ make_build() {
 		sleep 1
 	done
 
+	[[ $1 == "Arch"* ]] && (ssh $1 ls /usr/lib/{libncursesw.a,libcpuid.a,libpci.a,libprocps.a} || exit 2)
 	ssh $1 << EOF
 
-err() {
-	echo -e "\n\t\033[1;41m*** A build failed for $1 ***\033[0m\n\n"
-	sleep 5
+makeopts() {
+	if make $JOBS; then
+		echo -e "\n\t\033[1;42m*** Build passed for $1 ***\033[0m\n\n"
+		sleep 2
+	else
+		echo -e "\n\t\033[1;41m*** Build failed for $1 ***\033[0m\n\n"
+		read
+	fi
 }
 
 [[ ! -d $DIR ]] && git clone https://github.com/X0rg/CPU-X $DIR
-mkdir -pv $DIR/{,e}build{1,2,3,4,5,6,7}
+mkdir -pv $DIR/{,e}build{1,2,3,4,5,6,7,8}
 
-cd $DIR/build1 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr .. && make $JOBS || err
-cd $DIR/build2 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=0 .. && make $JOBS || err
-cd $DIR/build3 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=1 -DWITH_NCURSES=0 .. && make $JOBS || err
-cd $DIR/build4 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NCURSES=1 -DWITH_LIBCPUID=0 .. && make $JOBS || err
-cd $DIR/build5 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBCPUID=1 -DWITH_LIBDMI=0 .. && make $JOBS || err
-cd $DIR/build6 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBDMI=1 -DWITH_LIBPCI=0 .. && make $JOBS || err
-cd $DIR/build7 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBPCI=1 -DWITH_LIBSYSTEM=0 .. && make $JOBS || err
+sleep 5
+echo -e "\n\n\033[1;44m*** Start normal build for $1\033[0m\n"
+cd $DIR/build1 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr .. &&
+cd $DIR/build2 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=0 .. && makeopts
+cd $DIR/build3 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=1 -DWITH_NCURSES=0 .. && makeopts
+cd $DIR/build4 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NCURSES=1 -DWITH_LIBCPUID=0 .. && makeopts
+cd $DIR/build5 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBCPUID=1 -DWITH_LIBDMI=0 .. && makeopts
+cd $DIR/build6 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBDMI=1 -DWITH_LIBBDWT=0 .. && makeopts
+cd $DIR/build7 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBBDWT=1 -DWITH_LIBPCI=0 .. && makeopts
+cd $DIR/build8 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBPCI=1 -DWITH_LIBSYSTEM=0 .. && makeopts
 
-cd $DIR/ebuild1 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 .. && make $JOBS || err
-cd $DIR/ebuild2 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 -DWITH_GTK=0 .. && make $JOBS || err
-cd $DIR/ebuild3 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 -DWITH_GTK=1 -DWITH_NCURSES=0 .. && make $JOBS || err
-cd $DIR/ebuild4 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 -DWITH_NCURSES=1 -DWITH_LIBCPUID=0 .. && make $JOBS || err
-cd $DIR/ebuild5 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 -DWITH_LIBCPUID=1 -DWITH_LIBDMI=0 .. && make $JOBS || err
-cd $DIR/ebuild6 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 -DWITH_LIBDMI=1 -DWITH_LIBPCI=0 .. && make $JOBS || err
-cd $DIR/ebuild7 && cmake -DCMAKE_BUILD_TYPE=Debug -EMBED=1 -DWITH_LIBPCI=1 -DWITH_LIBSYSTEM=0 .. && make $JOBS || err
+sleep 5
+echo -e "\n\n\033[1;44m*** Start embeded build for $1\033[0m\n"
+cd $DIR/ebuild1 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 .. && makeopts
+cd $DIR/ebuild2 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_GTK=0 .. && makeopts
+cd $DIR/ebuild3 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_GTK=1 -DWITH_NCURSES=0 .. && makeopts
+cd $DIR/ebuild4 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_NCURSES=1 -DWITH_LIBCPUID=0 .. && makeopts
+cd $DIR/ebuild5 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_LIBCPUID=1 -DWITH_LIBDMI=0 .. && makeopts
+cd $DIR/ebuild6 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_LIBDMI=1 -DWITH_LIBBDWT=0 .. && makeopts
+cd $DIR/ebuild7 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_LIBBDWT=1 -DWITH_LIBPCI=0 .. && makeopts
+cd $DIR/ebuild8 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_LIBPCI=1 -DWITH_LIBSYSTEM=0 .. && makeopts
 
 poweroff || sudo poweroff
 EOF
@@ -52,14 +64,25 @@ make_release() {
 	[[ $1 == "Arch"* ]] && (ssh $1 ls /usr/lib/{libncursesw.a,libcpuid.a,libpci.a,libprocps.a} || exit 2)
 	ssh $1 << EOF
 
+makeopts() {
+	if make $JOBS; then
+		echo -e "\n\t\033[1;42m*** Build passed for $1 ***\033[0m\n\n"
+	else
+		echo -e "\n\t\033[1;41m*** Build failed for $1 ***\033[0m\n\n"
+		exit
+	fi
+}
+
 [[ ! -d $DIR ]] && git clone https://github.com/X0rg/CPU-X $DIR
 mkdir -pv $DIR/{,g}n_build
 cd $DIR && VER=\$(git tag | tail -n1)
 
-cd $DIR/gn_build && cmake -DCMAKE_BUILD_TYPE=Release -DEMBED=1 .. && make $JOBS
-cd $DIR/n_build  && cmake -DCMAKE_BUILD_TYPE=Release -DEMBED=1 -DWITH_GTK=0 .. && make $JOBS
+cd $DIR/gn_build && cmake -DCMAKE_BUILD_TYPE=Release -DEMBED=1 .. && makeopts
+cd $DIR/n_build  && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_GTK=0 .. && makeopts
 
-[[ $1 == "Arch32" ]] && ARCH="linux32" || ARCH="linux64"
+[[ $1 != "Arch"*  ]] && exit
+[[ $1 == "Arch32" ]] && ARCH="linux32"
+[[ $1 == "Arch64" ]] && ARCH="linux64"
 cp -v $DIR/gn_build/accomplished/bin/cpu-x "/mnt/Echange/CPU-X_\${VER}_portable.\$ARCH"
 cp -v $DIR/n_build/accomplished/bin/cpu-x  "/mnt/Echange/CPU-X_\${VER}_portable_noGTK.\$ARCH"
 EOF
