789a051d1a69be11839f57515e51c2e389010054
Xorg <xorgbreaker@gmail.com>
2015-12-12 16:24:07 +0100

    Update make_builds.sh script

--- make_builds.sh
+++ make_builds.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This script helps to track build troubles and make portable versions
 
 DIR=/tmp/CPU-X
@@ -23,36 +23,37 @@ makeopts() {
 		sleep 2
 	else
 		echo -e "\n\t\033[1;41m*** Build failed for $1 ***\033[0m\n\n"
-		read
+		sleep 10
 	fi
 }
 
-[[ ! -d $DIR ]] && git clone https://github.com/X0rg/CPU-X $DIR
-mkdir -pv $DIR/{,e}build{1,2,3,4,5,6,7,8}
+#[[ ! -d $DIR ]] && git clone https://github.com/X0rg/CPU-X $DIR
+cp -Rv /mnt/Echange/CPU-X $DIR
+mkdir -pv $DIR/{,e}build{1..9}
 
 sleep 5
 echo -e "\n\n\033[1;44m*** Start normal build for $1\033[0m\n"
-cd $DIR/build1 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr .. &&
-cd $DIR/build2 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=0 .. && makeopts
-cd $DIR/build3 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=1 -DWITH_NCURSES=0 .. && makeopts
-cd $DIR/build4 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NCURSES=1 -DWITH_LIBCPUID=0 .. && makeopts
-cd $DIR/build5 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBCPUID=1 -DWITH_DMIDECODE=0 .. && makeopts
-cd $DIR/build6 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_DMIDECODE=1 -DWITH_BANDWIDTH=0 .. && makeopts
-cd $DIR/build7 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_BANDWIDTH=1 -DWITH_LIBPCI=0 .. && makeopts
-cd $DIR/build8 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBPCI=1 -DWITH_LIBSYSTEM=0 .. && makeopts
+cd $DIR/build1 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr                    .. && makeopts
+cd $DIR/build2 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=0       .. && makeopts
+cd $DIR/build3 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NCURSES=0   .. && makeopts
+cd $DIR/build4 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GETTEXT=0   .. && makeopts
+cd $DIR/build5 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBCPUID=0  .. && makeopts
+cd $DIR/build6 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBPCI=0    .. && makeopts
+cd $DIR/build7 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBSYSTEM=0 .. && makeopts
+cd $DIR/build8 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_DMIDECODE=0 .. && makeopts
+cd $DIR/build9 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_BANDWIDTH=0 .. && makeopts
 
 sleep 5
-echo -e "\n\n\033[1;44m*** Start embeded build for $1\033[0m\n"
-cd $DIR/ebuild1 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 .. && makeopts
-cd $DIR/ebuild2 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_GTK=0 .. && makeopts
-cd $DIR/ebuild3 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_GTK=1 -DWITH_NCURSES=0 .. && makeopts
-cd $DIR/ebuild4 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_NCURSES=1 -DWITH_LIBCPUID=0 .. && makeopts
-cd $DIR/ebuild5 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_LIBCPUID=1 -DWITH_DMIDECODE=0 .. && makeopts
-cd $DIR/ebuild6 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_DMIDECODE=1 -DWITH_BANDWIDTH=0 .. && makeopts
-cd $DIR/ebuild7 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_BANDWIDTH=1 -DWITH_LIBPCI=0 .. && makeopts
-cd $DIR/ebuild8 && cmake -DCMAKE_BUILD_TYPE=Debug -DEMBED=1 -DWITH_LIBPCI=1 -DWITH_LIBSYSTEM=0 .. && makeopts
-
-poweroff || sudo poweroff
+echo -e "\n\n\033[1;44m*** Start portable build for $1\033[0m\n"
+cd $DIR/ebuild1 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1                    .. && makeopts
+cd $DIR/ebuild2 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_GTK=0       .. && makeopts
+cd $DIR/ebuild3 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_NCURSES=0   .. && makeopts
+cd $DIR/ebuild4 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_GETTEXT=0   .. && makeopts
+cd $DIR/ebuild5 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBCPUID=0  .. && makeopts
+cd $DIR/ebuild6 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBPCI=0    .. && makeopts
+cd $DIR/ebuild7 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBSYSTEM=0 .. && makeopts
+cd $DIR/ebuild8 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_DMIDECODE=0 .. && makeopts
+cd $DIR/ebuild9 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_BANDWIDTH=0 .. && makeopts
 EOF
 }
 
@@ -77,15 +78,21 @@ makeopts() {
 mkdir -pv $DIR/{,g}n_build
 cd $DIR && VER=\$(git tag | tail -n1)
 
-cd $DIR/gn_build && cmake -DCMAKE_BUILD_TYPE=Release -DEMBED=1 .. && makeopts
-cd $DIR/n_build  && cmake -DCMAKE_BUILD_TYPE=Release -DEMBED=1 -DWITH_GTK=0 .. && makeopts
+cd $DIR/gn_build && cmake -DCMAKE_BUILD_TYPE=Release -DPORTABLE_BINARY=1              .. && makeopts
+cd $DIR/n_build  && cmake -DCMAKE_BUILD_TYPE=Release -DPORTABLE_BINARY=1 -DWITH_GTK=0 .. && makeopts
 
 [[ $1 != "Arch"*  ]] && exit
 [[ $1 == "Arch32" ]] && ARCH="linux32"
 [[ $1 == "Arch64" ]] && ARCH="linux64"
-cp -v $DIR/gn_build/accomplished/bin/cpu-x "/mnt/Echange/CPU-X_\${VER}_portable.\$ARCH"
-cp -v $DIR/n_build/accomplished/bin/cpu-x  "/mnt/Echange/CPU-X_\${VER}_portable_noGTK.\$ARCH"
+cp -v $DIR/gn_build/cpu-x "/mnt/Echange/CPU-X_\${VER}_portable.\$ARCH"
+cp -v $DIR/n_build/cpu-x  "/mnt/Echange/CPU-X_\${VER}_portable_noGTK.\$ARCH"
 EOF
+}
+
+stop_vms() {
+	ssh Arch32 sudo poweroff
+	ssh Arch64 sudo poweroff
+	ssh BSD    sudo poweroff
 }
 
 help() {
@@ -117,22 +124,19 @@ fi
 if [[ $choice == "build" ]]; then
 	# Start VMs
 	VBoxManage list runningvms | grep -q "Arch Linux i686"   || (echo "Start 32-bit Linux VM" ; VBoxHeadless --startvm "Arch Linux i686" &)
-	VBoxManage list runningvms | grep -q "GhostBSD" || (echo "Start 32-bit BSD VM" ; VBoxHeadless --startvm "GhostBSD" &)
-	VBoxManage list runningvms | grep -q "OS X" || (echo "Start 64-bit OS X VM" ; VBoxHeadless --startvm "OS X" &)
+	VBoxManage list runningvms | grep -q "GhostBSD"          || (echo "Start 32-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD" &)
 	sleep 5
 
 	# Start build
 	make_build Arch32
 	echo "Press a key to continue..." ; read
 	make_build BSD
-	echo "Press a key to continue..." ; read
-	make_build OSX
 
 elif [[ $choice == "release" ]]; then
 	# Start VMs
 	VBoxManage list runningvms | grep -q "Arch Linux i686"   || (echo "Start 32-bit Linux VM" ; VBoxHeadless --startvm "Arch Linux i686" &)
 	VBoxManage list runningvms | grep -q "Arch Linux x86_64" || (echo "Start 64-bit Linux VM" ; VBoxHeadless --startvm "Arch Linux x86_64" &)
-	VBoxManage list runningvms | grep -q "GhostBSD" || (echo "Start 32-bit BSD VM" ; VBoxHeadless --startvm "GhostBSD" &)
+	VBoxManage list runningvms | grep -q "GhostBSD"          || (echo "Start 32-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD" &)
 	sleep 5
 
 	# Start build
@@ -141,22 +145,21 @@ elif [[ $choice == "release" ]]; then
 	make_release BSD
 	sshfs BSD:/tmp/CPU-X/ ~/.VirtualBox/Echange/SSHFS
 
-	# Stop Linux VMs
-	ssh Arch32 sudo poweroff
-	ssh Arch64 sudo poweroff
-
 	# Get BSD ELF
 	cd ~/.VirtualBox/Echange/SSHFS
 	VER=$(git tag | tail -n1)
 	cd ..
-	cp -v SSHFS/gn_build/accomplished/bin/cpu-x "./CPU-X_${VER}_portable.bsd32"
-	cp -v SSHFS/n_build/accomplished/bin/cpu-x  "./CPU-X_${VER}_portable_noGTK.bsd32"
+	cp -v SSHFS/gn_build/cpu-x "./CPU-X_${VER}_portable.bsd32"
+	cp -v SSHFS/n_build/cpu-x  "./CPU-X_${VER}_portable_noGTK.bsd32"
 	fusermount -u SSHFS
-	ssh BSD sudo poweroff
 
 	# Make tarball
-	tar -zcvf CPU-X_${VER}_portable.tar.gz CPU-X_${VER}_portable.*
+	tar -zcvf CPU-X_${VER}_portable.tar.gz       CPU-X_${VER}_portable.*
 	tar -zcvf CPU-X_${VER}_portable_noGTK.tar.gz CPU-X_${VER}_portable_noGTK.*
 else
 	echo "Bad option."
 fi
+
+echo "Shutdown VMs (y/N)?"
+read -n1 s
+[[ $s == "y" ]] && stop_vms
