8fd8907031b28a1941ba7c88de11f8ca723638bf
wdlkmpx <wdlkmpx@users.noreply.github.com>
2016-04-11 07:21:57 +0000

    CPU Technology: add some Intel Atom processors (#24)
    https://en.wikipedia.org/wiki/List_of_Intel_Atom_microprocessors
    
    Screenshots:
    http://s000.tinyupload.com/index.php?file_id=01921619395342213468

--- src/core.c
+++ src/core.c
@@ -190,17 +190,22 @@ static int cpu_technology(Labels *data)
 		{  3,           3,          -1,          90 }, // P4 Prescott
 		{  4,           4,          -1,          90 }, // P4 Prescott/Irwindale / PD Smithfield
 		{  5,          37,          -1,          32 }, // Westmere
+		{  5,          53,          -1,          32 }, // Atom Cloverview
 		{  5,          69,          -1,          22 }, // Haswell
 		{  6,           6,          -1,          65 }, // P4 Cedar Mill / PD Presler
 		{  6,          22,          -1,          65 }, // C2 Conroe-L
+		{  6,          54,          -1,          32 }, // Atom Cedarview
 		{  7,          23,          -1,          45 }, // C2 Wolfdale / Yorkfield / Penryn
+		{  7,          55,          -1,          22 }, // Atom Bay Trail
 		{  7,          71,          -1,          14 }, // Broadwell
 		{ 10,          26,          -1,          45 }, // Nehalem
 		{ 10,          30,          -1,          45 }, // Nehalem
 		{ 10,          42,          -1,          32 }, // Sandy Bridge
 		{ 10,          58,          -1,          22 }, // Ivy Bridge
+		{ 12,          28,          -1,          45 }, // Atom Diamondville / Pineview / Silverthorne
 		{ 12,          44,          -1,          32 }, // Westmere
 		{ 12,          60,          -1,          22 }, // Haswell
+		{ 12,          76,          -1,          14 }, // Atom Cherry Trail
 		{ 13,          45,          -1,          32 }, // Sandy Bridge-E
 		{ 13,          61,          -1,          14 }, // Broadwell
 		{ 14,          14,          -1,          65 }, // Yonah (Core Solo)
