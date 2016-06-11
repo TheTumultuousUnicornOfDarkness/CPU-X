TheTumultuousUnicornOfDarkness/CPU-X

commit 0ba897215dac21b436462d20602615a5cb95f91c

Move some bandwidth-specific stuff in a substructure It allows to remove superficial functions designed to libbandwidth

src/bandwidth/bandwidth.c
@@ -19,6 +19,7 @@
   The author may be reached at veritas@comcast.net.
  *===========================================================================*/
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -1819,7 +1820,7 @@ usage ()
 //----------------------------------------------------------------------------
 // Name:	main
 //----------------------------------------------------------------------------
-int bandwidth(Labels *data)
+int call_bandwidth(Labels *data)
 {
 	int i, chunk_size = 0;
 	uint32_t cache_size = 0;
@@ -1993,21 +1994,29 @@ int bandwidth(Labels *data)
 	goto end_initialization;
 
 fast_initialization:
-	if(data->l1_size < 1)
+	if(data->w_data->l1_size < 1)
 		return 1;
 
 	if(opts->bw_test >= LASTTEST)
 	{
 		MSG_ERROR(_("Invalid bandwidth test selectionned!"));
 		MSG_ERROR(_("Available tests:"));
-		for(i = 0; i < LASTTEST; i++)
+		for(i = SEQ_128_R; i < LASTTEST; i++)
 			fprintf(stderr, "\t%i: %s\n", i, tests[i].name);
 		opts->bw_test = 0;
 	}
 
+	if(data->w_data->test_count != 0)
+	{
+		data->w_data->test_count = LASTTEST;
+		data->w_data->test_name  = malloc(LASTTEST * sizeof(char *));
+		for(i = SEQ_128_R; i < LASTTEST; i++)
+			asprintf(&data->w_data->test_name[i], "#%2i: %s", i, tests[i].name);
+	}
+
 	short cache_level = LEVEL1I;
 	int   count       = 0;
-	cache_size        = data->l1_size;
+	cache_size        = data->w_data->l1_size;
 
 	if(data->cpu_vendor_id == 1)
 	{
@@ -2492,4 +2501,9 @@ end_initialization:
 	}
 
 	return 0;
+}
+
+int run_bandwidth()
+{
+	return call_bandwidth(NULL);
 }

src/bandwidth/libbandwidth.h
@@ -38,7 +38,7 @@ if(!BANDWIDTH_MODE && chunk_size > cache_size * 1024) \
 	if(cache_level > LEVEL3) \
 		return 0; \
 \
-	cache_size = (cache_level == LEVEL2) ? data->l2_size : data->l3_size; \
+	cache_size = (cache_level == LEVEL2) ? data->w_data->l2_size : data->w_data->l3_size; \
 	if(cache_size < 1) \
 		return 1; \
 }

src/core.c
@@ -51,10 +51,6 @@
 # include "dmidecode/dmiopt.h"
 #endif
 
-#if HAS_BANDWIDTH
-# include "bandwidth/libbandwidth.h"
-#endif
-
 #if HAS_LIBPCI
 # include "pci/pci.h"
 #endif
@@ -82,7 +78,7 @@ int fill_labels(Labels *data)
 	if(HAS_LIBCPUID)     err += cpu_multipliers(data);
 	if(HAS_LIBPROCPS)    err += system_dynamic(data);
 	if(HAS_LIBSTATGRAB)  err += system_dynamic(data);
-	if(HAS_BANDWIDTH)    err += bandwidth(data);
+	if(HAS_BANDWIDTH)    err += call_bandwidth(data);
 	if(HAS_LIBPCI)       find_devices(data);
 	err += gpu_temperature(data);
 	err += system_static(data);
@@ -109,7 +105,7 @@ int do_refresh(Labels *data, enum EnTabNumber page)
 			cpu_usage(data, -1);
 			break;
 		case NO_CACHES:
-			if(HAS_BANDWIDTH)    err = bandwidth(data);
+			if(HAS_BANDWIDTH)    err = call_bandwidth(data);
 			break;
 		case NO_SYSTEM:
 			if(HAS_LIBPROCPS)    err = system_dynamic(data);
@@ -127,49 +123,6 @@ int do_refresh(Labels *data, enum EnTabNumber page)
 
 	return err;
 }
-
-#if HAS_DMIDECODE
-/* Call Dmidecode through CPU-X but do nothing else */
-int run_dmidecode(void)
-{
-	opt.type  = NULL;
-	opt.flags = (opts->verbose) ? 0 : FLAG_QUIET;
-	return dmidecode();
-}
-#endif /* HAS_DMIDECODE */
-
-#if HAS_BANDWIDTH
-/* Call Bandwidth through CPU-X but do nothing else */
-int run_bandwidth(void)
-{
-	return bandwidth(NULL);
-}
-
-/* Get string for selected bandwidth test */
-char *bandwidth_test_name(unsigned int test)
-{
-	static char *name = NULL;
-	asprintf(&name, "#%2i: %s", test, tests[test].name);
-	return name;
-}
-
-/* Get bandwidth count tests */
-int bandwidth_last_test(void)
-{
-	return LASTTEST;
-}
-
-#else
-char *bandwidth_test_name(unsigned int test)
-{
-	return '\0';
-}
-
-int bandwidth_last_test(void)
-{
-	return 0;
-}
-#endif /* HAS_BANDWIDTH */
 
 
 /************************* Private functions *************************/
@@ -381,7 +334,7 @@ static int call_libcpuid_static(Labels *data)
 	/* Cache level 1 (instruction) */
 	if(datanr.l1_instruction_cache > 0)
 	{
-		data->l1_size = datanr.l1_instruction_cache;
+		data->w_data->l1_size = datanr.l1_instruction_cache;
 		iasprintf(&data->tab_cpu[VALUE][LEVEL1I], "%d x %4d KB, %2d-way", datanr.num_cores, datanr.l1_instruction_cache, datanr.l1_assoc);
 		iasprintf(&data->tab_caches[VALUE][L1SIZE], data->tab_cpu[VALUE][LEVEL1I]);
 		iasprintf(&data->tab_caches[VALUE][L1DESCRIPTOR], fmt, datanr.l1_assoc, datanr.l1_cacheline);
@@ -390,7 +343,7 @@ static int call_libcpuid_static(Labels *data)
 	/* Cache level 2 */
 	if(datanr.l2_cache > 0)
 	{
-		data->l2_size = datanr.l2_cache;
+		data->w_data->l2_size = datanr.l2_cache;
 		iasprintf(&data->tab_cpu[VALUE][LEVEL2], "%d x %4d KB, %2d-way", datanr.num_cores, datanr.l2_cache, datanr.l2_assoc);
 		iasprintf(&data->tab_caches[VALUE][L2SIZE], data->tab_cpu[VALUE][LEVEL2]);
 		iasprintf(&data->tab_caches[VALUE][L2DESCRIPTOR], fmt, datanr.l2_assoc, datanr.l2_cacheline);
@@ -399,7 +352,7 @@ static int call_libcpuid_static(Labels *data)
 	/* Cache level 3 */
 	if(datanr.l3_cache > 0)
 	{
-		data->l3_size = datanr.l3_cache;
+		data->w_data->l3_size = datanr.l3_cache;
 		iasprintf(&data->tab_cpu[VALUE][LEVEL3], "%9d KB, %2d-way", datanr.l3_cache, datanr.l3_assoc);
 		iasprintf(&data->tab_caches[VALUE][L3SIZE], data->tab_cpu[VALUE][LEVEL3]);
 		iasprintf(&data->tab_caches[VALUE][L3DESCRIPTOR], fmt, datanr.l3_assoc, datanr.l3_cacheline);
@@ -551,6 +504,14 @@ static int call_libcpuid_dynamic(Labels *data)
 #endif /* HAS_LIBCPUID */
 
 #if HAS_DMIDECODE
+/* Call Dmidecode through CPU-X but do nothing else */
+int run_dmidecode(void)
+{
+	opt.type  = NULL;
+	opt.flags = (opts->verbose) ? 0 : FLAG_QUIET;
+	return dmidecode();
+}
+
 /* Elements provided by dmidecode (need root privileges) */
 static int call_dmidecode(Labels *data)
 {

src/core.h
@@ -42,6 +42,10 @@ static int call_libcpuid_dynamic(Labels *data);
 static int call_dmidecode(Labels *data);
 /* Required: HAS_DMIDECODE && root privileges */
 
+/* Call bandwidth library */
+int call_bandwidth(Labels *data);
+/* Required: HAS_BANDWIDTH */
+
 /* Get CPU multipliers ("x current (min-max)" label) */
 static int cpu_multipliers(Labels *data);
 /* Required: HAS_LIBCPUID */

src/cpu-x.h
@@ -157,6 +157,13 @@ enum EnTabAbout
 
 typedef struct
 {
+	uint8_t  test_count;
+	uint32_t l1_size, l2_size, l3_size;
+	char     **test_name;
+} BandwidthData;
+
+typedef struct
+{
 	bool     run, fast_mode;
 	unsigned duration, threads;
 	uint32_t primes;
@@ -182,9 +189,10 @@ typedef struct
 	int8_t   cpu_vendor_id;
 	uint8_t  cpu_count, gpu_count, dimms_count;
 	int32_t  cpu_model, cpu_ext_model, cpu_ext_family;
-	uint32_t l1_size, l2_size, l3_size;
 	double   bus_freq, min_mult, max_mult;
-	BenchData *b_data;
+
+	BandwidthData *w_data;
+	BenchData     *b_data;
 } Labels;
 
 typedef struct
@@ -241,15 +249,6 @@ int run_dmidecode(void);
 
 /* Call Bandwidth through CPU-X but do nothing else */
 int run_bandwidth(void);
-
-/* Get string for selected bandwidth test */
-char *bandwidth_test_name(unsigned int test);
-
-/* Get bandwidth count tests */
-int bandwidth_last_test(void);
-
-/* Call bandwidth library */
-int bandwidth(Labels *data);
 
 /* Perform a multithreaded benchmark (compute prime numbers) */
 void start_benchmarks(Labels *data);

src/gui_gtk.c
@@ -191,7 +191,7 @@ static void change_activetest(GtkComboBox *box, Labels *data)
 {
 	const gint test = gtk_combo_box_get_active(GTK_COMBO_BOX(box));
 
-	if(0 <= test && test < bandwidth_last_test())
+	if(0 <= test && test < data->w_data->test_count)
 		opts->bw_test = test;
 }
 
@@ -464,8 +464,8 @@ static void set_labels(GtkLabels *glab, Labels *data)
 		gtk_label_set_text(GTK_LABEL(glab->gtktab_caches[NAME][i]),  data->tab_caches[NAME][i]);
 		gtk_label_set_text(GTK_LABEL(glab->gtktab_caches[VALUE][i]), data->tab_caches[VALUE][i]);
 	}
-	for(i = 0; i < bandwidth_last_test(); i++)
-		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(glab->activetest), bandwidth_test_name(i));
+	for(i = 0; i < data->w_data->test_count; i++)
+		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(glab->activetest), data->w_data->test_name[i]);
 	gtk_combo_box_set_active(GTK_COMBO_BOX(glab->activetest), opts->bw_test);
 
 	/* Tab Motherboard */

src/main.c
@@ -654,8 +654,9 @@ int main(int argc, char *argv[])
 	                .tab_memory = {{ NULL }}, .tab_system     = {{ NULL }}, .tab_graphics    = {{ NULL }},
 	                .cpu_freq   = 0,          .cpu_vendor_id  = 0,          .bus_freq        = 0.0,
 	                .cpu_count  = 0,          .gpu_count      = 0,          .dimms_count     = 0,
-	                .l1_size    = 0,          .l2_size        = 0,          .l3_size         = 0,
 			.min_mult   = -1,         .max_mult       = -1 };
+
+	data->w_data = &(BandwidthData) { .l1_size = 0, .test_count = 0, .test_name = NULL };
 
 	data->b_data = &(BenchData) { .run = false, .duration = 1, .threads = 1, .primes = 0 };
 

src/tui_ncurses.c
@@ -134,7 +134,7 @@ void start_tui_ncurses(Labels *data)
 				else if(page == NO_CACHES && opts->bw_test > 0)
 				{
 					opts->bw_test--;
-					print_activetest(win);
+					print_activetest(win, data);
 				}
 				else if(page == NO_BENCH && data->b_data->duration > 1)
 				{
@@ -155,10 +155,10 @@ void start_tui_ncurses(Labels *data)
 					opts->selected_core++;
 					print_activecore(win);
 				}
-				else if(page == NO_CACHES && (int) opts->bw_test < bandwidth_last_test() - 1)
+				else if(page == NO_CACHES && (int) opts->bw_test < data->w_data->test_count - 1)
 				{
 					opts->bw_test++;
-					print_activetest(win);
+					print_activetest(win, data);
 				}
 				else if(page == NO_BENCH && data->b_data->duration < 60 * 24)
 				{
@@ -517,10 +517,10 @@ static void ntab_cpu(WINDOW *win, const SizeInfo info, Labels *data)
 }
 
 /* Display active Test in Caches tab */
-static void print_activetest(WINDOW *win)
+static void print_activetest(WINDOW *win, Labels *data)
 {
 	if(HAS_BANDWIDTH)
-		mvwprintwc(win, LINE_16, 12, DEFAULT_COLOR, "%s", bandwidth_test_name(opts->bw_test));
+		mvwprintwc(win, LINE_16, 12, DEFAULT_COLOR, "%s", data->w_data->test_name[opts->bw_test]);
 }
 
 /* Caches tab */
@@ -548,7 +548,7 @@ static void ntab_caches(WINDOW *win, const SizeInfo info, Labels *data)
 
 	/* Test frame */
 	frame(win, LINE_15, info.start , LINE_17, info.width - 1, data->objects[FRAMTEST]);
-	print_activetest(win);
+	print_activetest(win, data);
 
 	wrefresh(win);
 }

commit 0660541573f02c4b0c790b2cf9b45e5c0a36401e

Fix a mistake introduced by 0ba897215dac21b436462d20602615a5cb95f91c

src/bandwidth/bandwidth.c
@@ -2006,7 +2006,7 @@ fast_initialization:
 		opts->bw_test = 0;
 	}
 
-	if(data->w_data->test_count != 0)
+	if(data->w_data->test_count == 0)
 	{
 		data->w_data->test_count = LASTTEST;
 		data->w_data->test_name  = malloc(LASTTEST * sizeof(char *));