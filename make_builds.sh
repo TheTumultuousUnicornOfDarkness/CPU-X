e3d657b90b1ce4e86c358d5d4ba1d210e362349a
Xorg <xorgbreaker@gmail.com>
2016-06-15 18:56:00 +0200

    Update make_builds.sh script

--- make_builds.sh
+++ make_builds.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # This script helps to track build troubles and make portable versions
 
-DIR=/tmp/CPU-X
+VER=$(git tag | tail -n1)
+SRCDIR=/tmp/CPU-X
+DESTDIR="$(dirname $0)/portable"
 JOBS=-j2
+VMs=("Arch32"  "Arch64"  "BSD32" "BSD64")
+EXT=("linux32" "linux64" "bsd32" "bsd64")
 
 
 #########################################################
@@ -27,33 +31,31 @@ makeopts() {
 	fi
 }
 
-#[[ ! -d $DIR ]] && git clone https://github.com/X0rg/CPU-X $DIR
-cp -Rv /mnt/Echange/CPU-X $DIR
-mkdir -pv $DIR/{,e}build{1..9}
+[[ ! -d $SRCDIR ]] && git clone https://github.com/X0rg/CPU-X $SRCDIR || (cd $SRCDIR && git pull)
+mkdir -pv $SRCDIR/{,e}build{1..9}
 
-sleep 5
 echo -e "\n\n\033[1;44m*** Start normal build for $1\033[0m\n"
-cd $DIR/build1 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr                    .. && makeopts
-cd $DIR/build2 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=0       .. && makeopts
-cd $DIR/build3 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NCURSES=0   .. && makeopts
-cd $DIR/build4 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GETTEXT=0   .. && makeopts
-cd $DIR/build5 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBCPUID=0  .. && makeopts
-cd $DIR/build6 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBPCI=0    .. && makeopts
-cd $DIR/build7 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBSYSTEM=0 .. && makeopts
-cd $DIR/build8 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_DMIDECODE=0 .. && makeopts
-cd $DIR/build9 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_BANDWIDTH=0 .. && makeopts
+cd $SRCDIR/build1 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr                    .. && makeopts
+cd $SRCDIR/build2 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GTK=0       .. && makeopts
+cd $SRCDIR/build3 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_NCURSES=0   .. && makeopts
+cd $SRCDIR/build4 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_GETTEXT=0   .. && makeopts
+cd $SRCDIR/build5 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBCPUID=0  .. && makeopts
+cd $SRCDIR/build6 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBPCI=0    .. && makeopts
+cd $SRCDIR/build7 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_LIBSYSTEM=0 .. && makeopts
+cd $SRCDIR/build8 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_DMIDECODE=0 .. && makeopts
+cd $SRCDIR/build9 && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DWITH_BANDWIDTH=0 .. && makeopts
 
 sleep 5
 echo -e "\n\n\033[1;44m*** Start portable build for $1\033[0m\n"
-cd $DIR/ebuild1 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1                    .. && makeopts
-cd $DIR/ebuild2 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_GTK=0       .. && makeopts
-cd $DIR/ebuild3 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_NCURSES=0   .. && makeopts
-cd $DIR/ebuild4 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_GETTEXT=0   .. && makeopts
-cd $DIR/ebuild5 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBCPUID=0  .. && makeopts
-cd $DIR/ebuild6 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBPCI=0    .. && makeopts
-cd $DIR/ebuild7 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBSYSTEM=0 .. && makeopts
-cd $DIR/ebuild8 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_DMIDECODE=0 .. && makeopts
-cd $DIR/ebuild9 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_BANDWIDTH=0 .. && makeopts
+cd $SRCDIR/ebuild1 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1                    .. && makeopts
+cd $SRCDIR/ebuild2 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_GTK=0       .. && makeopts
+cd $SRCDIR/ebuild3 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_NCURSES=0   .. && makeopts
+cd $SRCDIR/ebuild4 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_GETTEXT=0   .. && makeopts
+cd $SRCDIR/ebuild5 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBCPUID=0  .. && makeopts
+cd $SRCDIR/ebuild6 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBPCI=0    .. && makeopts
+cd $SRCDIR/ebuild7 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_LIBSYSTEM=0 .. && makeopts
+cd $SRCDIR/ebuild8 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_DMIDECODE=0 .. && makeopts
+cd $SRCDIR/ebuild9 && cmake -DCMAKE_BUILD_TYPE=Debug -DPORTABLE_BINARY=1 -DWITH_BANDWIDTH=0 .. && makeopts
 EOF
 }
 
@@ -66,7 +68,7 @@ make_release() {
 	ssh $1 << EOF
 
 makeopts() {
-	if make $JOBS; then
+	if make $JOBS > /dev/null ; then
 		echo -e "\n\t\033[1;42m*** Build passed for $1 ***\033[0m\n\n"
 	else
 		echo -e "\n\t\033[1;41m*** Build failed for $1 ***\033[0m\n\n"
@@ -74,25 +76,18 @@ makeopts() {
 	fi
 }
 
-[[ ! -d $DIR ]] && git clone https://github.com/X0rg/CPU-X $DIR
-mkdir -pv $DIR/{,g}n_build
-cd $DIR && VER=\$(git tag | tail -n1)
+[[ ! -d $SRCDIR ]] && git clone https://github.com/X0rg/CPU-X $SRCDIR || (cd $SRCDIR && git pull)
+mkdir -pv $SRCDIR/{,g}n_build
 
-cd $DIR/gn_build && cmake -DCMAKE_BUILD_TYPE=Release -DPORTABLE_BINARY=1              .. && makeopts
-cd $DIR/n_build  && cmake -DCMAKE_BUILD_TYPE=Release -DPORTABLE_BINARY=1 -DWITH_GTK=0 .. && makeopts
-
-[[ $1 != "Arch"*  ]] && exit
-[[ $1 == "Arch32" ]] && ARCH="linux32"
-[[ $1 == "Arch64" ]] && ARCH="linux64"
-cp -v $DIR/gn_build/cpu-x "/mnt/Echange/CPU-X_\${VER}_portable.\$ARCH"
-cp -v $DIR/n_build/cpu-x  "/mnt/Echange/CPU-X_\${VER}_portable_noGTK.\$ARCH"
+cd $SRCDIR/gn_build && cmake -DCMAKE_BUILD_TYPE=Release -DPORTABLE_BINARY=1              .. > /dev/null && makeopts
+cd $SRCDIR/n_build  && cmake -DCMAKE_BUILD_TYPE=Release -DPORTABLE_BINARY=1 -DWITH_GTK=0 .. > /dev/null && makeopts
 EOF
 }
 
 stop_vms() {
-	ssh Arch32 sudo poweroff
-	ssh Arch64 sudo poweroff
-	ssh BSD    sudo poweroff
+	for i in {0..3}; do
+		ssh ${VMs[i]} sudo poweroff
+	done
 }
 
 help() {
@@ -124,36 +119,35 @@ fi
 if [[ $choice == "build" ]]; then
 	# Start VMs
 	VBoxManage list runningvms | grep -q "Arch Linux i686"   || (echo "Start 32-bit Linux VM" ; VBoxHeadless --startvm "Arch Linux i686" &)
-	VBoxManage list runningvms | grep -q "GhostBSD"          || (echo "Start 32-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD" &)
-	sleep 5
+	VBoxManage list runningvms | grep -q "GhostBSD i386"     || (echo "Start 32-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD i386" &)
+	sleep 1
 
 	# Start build
 	make_build Arch32
 	echo "Press a key to continue..." ; read
-	make_build BSD
+	make_build BSD32
 
 elif [[ $choice == "release" ]]; then
 	# Start VMs
 	VBoxManage list runningvms | grep -q "Arch Linux i686"   || (echo "Start 32-bit Linux VM" ; VBoxHeadless --startvm "Arch Linux i686" &)
 	VBoxManage list runningvms | grep -q "Arch Linux x86_64" || (echo "Start 64-bit Linux VM" ; VBoxHeadless --startvm "Arch Linux x86_64" &)
-	VBoxManage list runningvms | grep -q "GhostBSD"          || (echo "Start 32-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD" &)
-	sleep 5
+	VBoxManage list runningvms | grep -q "GhostBSD i386"     || (echo "Start 32-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD i386" &)
+	VBoxManage list runningvms | grep -q "GhostBSD x86_64"   || (echo "Start 64-bit BSD VM"   ; VBoxHeadless --startvm "GhostBSD x86_64" &)
+	sleep 1
 
 	# Start build
-	make_release Arch32
-	make_release Arch64
-	make_release BSD
-	sshfs BSD:/tmp/CPU-X/ ~/.VirtualBox/Echange/SSHFS
-
-	# Get BSD ELF
-	cd ~/.VirtualBox/Echange/SSHFS
-	VER=$(git tag | tail -n1)
-	cd ..
-	cp -v SSHFS/gn_build/cpu-x "./CPU-X_${VER}_portable.bsd32"
-	cp -v SSHFS/n_build/cpu-x  "./CPU-X_${VER}_portable_noGTK.bsd32"
-	fusermount -u SSHFS
+	[[ -d "$DESTDIR" ]] && rm -rf "$DESTDIR"
+	mkdir -pv "$DESTDIR/sshfs"
+	for i in {0..3}; do
+		make_release ${VMs[i]}
+		sshfs ${VMs[i]}:$SRCDIR "$DESTDIR/sshfs"
+		cp -v "$DESTDIR/sshfs/gn_build/cpu-x" "$DESTDIR/CPU-X_${VER}_portable.${EXT[i]}"
+		cp -v "$DESTDIR/sshfs/n_build/cpu-x"  "$DESTDIR/CPU-X_${VER}_portable_noGTK.${EXT[i]}"
+		fusermount -u "$DESTDIR/sshfs"
+	done
 
 	# Make tarball
+	cd "$DESTDIR"
 	tar -zcvf CPU-X_${VER}_portable.tar.gz       CPU-X_${VER}_portable.*
 	tar -zcvf CPU-X_${VER}_portable_noGTK.tar.gz CPU-X_${VER}_portable_noGTK.*
 else
