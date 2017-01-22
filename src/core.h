3452ecf128b2a98c656a2e1442c2b9e73c63e95c
Xorg <xorgbreaker@gmail.com>
2017-01-22 18:38:45 +0100

    Update copyright

--- src/core.h
+++ src/core.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-*    Copyright © 2014-2016 Xorg
+*    Copyright © 2014-2017 Xorg
 *
 *    This program is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
