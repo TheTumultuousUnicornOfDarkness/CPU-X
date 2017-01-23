TheTumultuousUnicornOfDarkness/CPU-X

commit 5f78a7f92dc9689aa9501af99deb70157bd32d66

Rewrite labels_setname() function

src/core.c
@@ -621,8 +621,8 @@ static int call_dmidecode(Labels *data)
 	if(data->tab_cpu[VALUE][BUSSPEED] != NULL)
 		data->bus_freq = strtod(data->tab_cpu[VALUE][BUSSPEED], NULL);
 
-	while(data->tab_memory[VALUE][data->dimm_count] != NULL)
-		data->dimm_count++;
+	//while(data->tab_memory[VALUE][data->dimm_count] != NULL) //FIXME
+	//	data->dimm_count++;
 
 	if(err)
 		MSG_ERROR(_("failed to call dmidecode"));

src/cpu-x.h
@@ -35,7 +35,9 @@
 #define PRGURL                "http://X0rg.github.io/CPU-X"
 #define UPDURL                "http://x0rg.github.io/CPU-X/version.txt"
 #define TARBALL               "https://github.com/"PRGAUTH"/"PRGNAME"/releases/download"
-#define PRGCPYR               "Copyright © 2014-2016 Xorg"
+#define PRGCPRGHT             "Copyright © 2014-2016 Xorg"
+#define PRGLCNS               "GNU GPL v3"
+#define LCNSURL               "http://www.gnu.org/licenses/gpl.txt"
 
 /* Colors definition */
 #define DEFAULT               "\x1b[0m"
@@ -52,6 +54,7 @@
 #define MSG_WARNING(fmt, ...) fprintf(stdout, msg_newline(BOLD_YELLOW, fmt), ##__VA_ARGS__)
 #define MSG_ERROR(fmt, ...)   fprintf(stderr, msg_error(BOLD_RED, BASEFILE, __LINE__, fmt), ##__VA_ARGS__)
 #define _(msg)                gettext(msg)
+#define N_(msg)               msg
 
 /* Options definition */
 #define OUT_GTK               (1 << 0)

src/label_names.h
@@ -1,0 +1,251 @@
+/****************************************************************************
+*    Copyright © 2014-2017 Xorg
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************************/
+
+/*
+* PROJECT CPU-X
+* FILE label_names.h
+*/
+
+#ifndef _LABEL_NAMES_H_
+#define _LABEL_NAMES_H_
+
+
+typedef struct
+{
+	const unsigned index;
+	const char *name;
+} LabelNames;
+
+typedef struct
+{
+	char **dim_names;
+	char **dim_values;
+	const LabelNames *names;
+	int last;
+} Arrays;
+
+const LabelNames objects_names[] =
+{
+	{ TABCPU,              N_("CPU")                  },
+	{ TABCACHES,           N_("Caches")               },
+	{ TABMOTHERBOARD,      N_("Motherboard")          },
+	{ TABMEMORY,           N_("Memory")               },
+	{ TABSYSTEM,           N_("System")               },
+	{ TABGRAPHICS,         N_("Graphics")             },
+	{ TABBENCH,            N_("Bench")                },
+	{ TABABOUT,            N_("About")                },
+
+	/* CPU tab */
+	{ FRAMPROCESSOR,       N_("Processor")            },
+	{ FRAMCLOCKS,          N_("Clocks")               },
+	{ FRAMCACHE,           N_("Cache")                },
+
+	/* Caches tab */
+	{ FRAML1CACHE,         N_("L1 Cache")             },
+	{ FRAML2CACHE,         N_("L2 Cache")             },
+	{ FRAML3CACHE,         N_("L3 Cache")             },
+	{ FRAML4CACHE,         N_("L4 Cache")             },
+	{ FRAMTEST,            N_("Test")                 },
+
+	/* Motherboard tab */
+	{ FRAMMOTHERBOARD,     N_("Motherboard")          },
+	{ FRAMBIOS,            N_("BIOS")                 },
+	{ FRAMCHIPSET,         N_("Chipset")              },
+
+	/* Memory tab */
+	{ FRAMBANK0,           N_("Bank 0")               },
+	{ FRAMBANK1,           N_("Bank 1")               },
+	{ FRAMBANK2,           N_("Bank 2")               },
+	{ FRAMBANK3,           N_("Bank 3")               },
+	{ FRAMBANK4,           N_("Bank 4")               },
+	{ FRAMBANK5,           N_("Bank 5")               },
+	{ FRAMBANK6,           N_("Bank 6")               },
+	{ FRAMBANK7,           N_("Bank 7")               },
+
+	/* System tab */
+	{ FRAMOPERATINGSYSTEM, N_("Operating System")     },
+	{ FRAMMEMORY,          N_("Memory")               },
+
+	/* Graphics tab */
+	{ FRAMGPU1,            N_("Card 1")               },
+	{ FRAMGPU2,            N_("Card 2")               },
+	{ FRAMGPU3,            N_("Card 3")               },
+	{ FRAMGPU4,            N_("Card 4")               },
+
+	/* Bench tab */
+	{ FRAMPRIMESLOW,       N_("Prime numbers (slow)") },
+	{ FRAMPRIMEFAST,       N_("Prime numbers (fast)") },
+	{ FRAMPARAM,           N_("Parameters")           },
+
+	/* About tab */
+	{ FRAMABOUT,           N_("About")                },
+	{ FRAMLICENSE,         N_("License")              },
+};
+
+const LabelNames tab_cpu_names[] =
+{
+	// Processor frame
+	{ VENDOR,        N_("Vendor")        },
+	{ CODENAME,      N_("Code Name")     },
+	{ PACKAGE,       N_("Package")       },
+	{ TECHNOLOGY,    N_("Technology")    },
+	{ VOLTAGE,       N_("Voltage")       },
+	{ SPECIFICATION, N_("Specification") },
+	{ FAMILY,        N_("Family")        },
+	{ EXTFAMILY,     N_("Ext. Family")   },
+	{ MODEL,         N_("Model")         },
+	{ EXTMODEL,      N_("Ext. Model")    },
+	{ TEMPERATURE,   N_("Temp.")         },
+	{ STEPPING,      N_("Stepping")      },
+	{ INSTRUCTIONS,  N_("Instructions")  },
+
+	// Clocks frame
+	{ CORESPEED,     N_("Core Speed")    },
+	{ MULTIPLIER,    N_("Multiplier")    },
+	{ BUSSPEED,      N_("Bus Speed")     },
+	{ USAGE,         N_("Usage")         },
+
+	// Cache frame
+	{ LEVEL1D,       N_("L1 Data")       },
+	{ LEVEL1I,       N_("L1 Inst.")      },
+	{ LEVEL2,        N_("Level 2")       },
+	{ LEVEL3,        N_("Level 3")       },
+
+	// Unnamed frame
+	{ SOCKETS,       N_("Socket(s)")     },
+	{ CORES,         N_("Core(s)")       },
+	{ THREADS,       N_("Thread(s)")     },
+};
+
+const LabelNames tab_caches_names[] =
+{
+	// L1 Cache frame
+	{ L1SIZE,  N_("Size")  },
+	{ L1SPEED, N_("Speed") },
+
+	// L2 Cache frame
+	{ L2SIZE,  N_("Size")  },
+	{ L2SPEED, N_("Speed") },
+
+	// L3 Cache frame
+	{ L3SIZE,  N_("Size")  },
+	{ L3SPEED, N_("Speed") },
+
+	// L4 Cache frame
+	{ L4SIZE,  N_("Size")  },
+	{ L4SPEED, N_("Speed") },
+};
+
+const LabelNames tab_motherboard_names[] =
+{
+	// Motherboard frame
+	{ MANUFACTURER, N_("Manufacturer") },
+	{ MBMODEL,      N_("Model")        },
+	{ REVISION,     N_("Revision")     },
+
+	// BIOS frame
+	{ BRAND,        N_("Brand")        },
+	{ BIOSVERSION,  N_("Version")      },
+	{ DATE,         N_("Date")         },
+	{ ROMSIZE,      N_("ROM Size")     },
+
+	// Chipset frame
+	{ CHIPVENDOR,   N_("Vendor")       },
+	{ CHIPMODEL,    N_("Model")        },
+};
+
+const LabelNames tab_memory_names[] =
+{
+	{ BANK0, N_("Reference") },
+	{ BANK1, N_("Reference") },
+	{ BANK2, N_("Reference") },
+	{ BANK3, N_("Reference") },
+	{ BANK4, N_("Reference") },
+	{ BANK5, N_("Reference") },
+	{ BANK6, N_("Reference") },
+	{ BANK7, N_("Reference") },
+};
+
+const LabelNames tab_system_names[] =
+{
+	// Operating System frame
+	{ KERNEL,       N_("Kernel")       },
+	{ DISTRIBUTION, N_("Distribution") },
+	{ HOSTNAME,     N_("Hostname")     },
+	{ UPTIME,       N_("Uptime")       },
+	{ COMPILER,     N_("Compiler")     },
+
+	// Memory frame
+	{ USED,         N_("Used")         },
+	{ BUFFERS,      N_("Buffers")      },
+	{ CACHED,       N_("Cached")       },
+	{ FREE,         N_("Free")         },
+	{ SWAP,         N_("Swap")         },
+};
+
+const LabelNames tab_graphics_names[] =
+{
+	// Card 1 frame
+	{ GPU1VENDOR,      N_("Vendor")      },
+	{ GPU1MODEL,       N_("Model")       },
+	{ GPU1TEMPERATURE, N_("Temperature") },
+
+	// Card 2 frame
+	{ GPU2VENDOR,      N_("Vendor")      },
+	{ GPU2MODEL,       N_("Model")       },
+	{ GPU2TEMPERATURE, N_("Temperature") },
+
+	// Card 3 frame
+	{ GPU3VENDOR,      N_("Vendor")      },
+	{ GPU3MODEL,       N_("Model")       },
+	{ GPU3TEMPERATURE, N_("Temperature") },
+
+	// Card 4 frame
+	{ GPU4VENDOR,      N_("Vendor")      },
+	{ GPU4MODEL,       N_("Model")       },
+	{ GPU4TEMPERATURE, N_("Temperature") },
+};
+
+const LabelNames tab_bench_names[] =
+{
+	// Prime numbers (slow) frame
+	{ PRIMESLOWSCORE, N_("Score")    },
+	{ PRIMESLOWRUN,   N_("Run")      },
+
+	// Prime numbers (fast) frame
+	{ PRIMEFASTSCORE, N_("Score")    },
+	{ PRIMEFASTRUN,   N_("Run")      },
+
+	// Parameters frame
+	{ PARAMDURATION,  N_("Duration") },
+	{ PARAMTHREADS,   N_("Threads")  },
+};
+
+const LabelNames tab_about_names[] =
+{
+	{ DESCRIPTION, PRGNAME N_(" is a Free software that gathers information\non CPU, motherboard and more.") },
+	{ VERSIONSTR,  N_("Version ") PRGVER                                                                     },
+	{ AUTHOR,      N_("Author: ") PRGAUTH                                                                    },
+	{ SITE,        N_("Site: ") PRGURL                                                                       },
+	{ COPYRIGHT,   PRGCPRGHT                                                                                 },
+	{ LICENSE,     N_("This software is distributed under the terms of ") PRGLCNS                            },
+	{ NOWARRANTY,  N_("This program comes with ABSOLUTELY NO WARRANTY")                                      },
+};
+
+
+#endif /* _LABEL_NAMES_H_ */

src/main.c
@@ -34,6 +34,7 @@
 #include <locale.h>
 #include <libintl.h>
 #include "cpu-x.h"
+#include "label_names.h"
 
 #if HAS_LIBCURL
 # include <curl/curl.h>
@@ -57,166 +58,30 @@ Options *opts;
 /* Set labels name */
 static void labels_setname(Labels *data)
 {
-	int i, j;
-
-	MSG_VERBOSE(_("Setting label names"));
-
-	/* CPU tab */
-	asprintf(&data->objects[TABCPU],                _("CPU")); // Tab label
-	asprintf(&data->objects[FRAMPROCESSOR],         _("Processor")); // Frame label
-	asprintf(&data->tab_cpu[NAME][VENDOR],          _("Vendor"));
-	asprintf(&data->tab_cpu[NAME][CODENAME],        _("Code Name"));
-	asprintf(&data->tab_cpu[NAME][PACKAGE],         _("Package"));
-	asprintf(&data->tab_cpu[NAME][TECHNOLOGY],      _("Technology"));
-	asprintf(&data->tab_cpu[NAME][VOLTAGE],         _("Voltage"));
-	asprintf(&data->tab_cpu[NAME][SPECIFICATION],   _("Specification"));
-	asprintf(&data->tab_cpu[NAME][FAMILY],          _("Family"));
-	asprintf(&data->tab_cpu[NAME][EXTFAMILY],       _("Ext. Family"));
-	asprintf(&data->tab_cpu[NAME][MODEL],           _("Model"));
-	asprintf(&data->tab_cpu[NAME][EXTMODEL],        _("Ext. Model"));
-	asprintf(&data->tab_cpu[NAME][TEMPERATURE],     _("Temp."));
-	asprintf(&data->tab_cpu[NAME][STEPPING],        _("Stepping"));
-	asprintf(&data->tab_cpu[NAME][INSTRUCTIONS],    _("Instructions"));
-
-	asprintf(&data->objects[FRAMCLOCKS],            _("Clocks")); // Frame label
-	asprintf(&data->tab_cpu[NAME][CORESPEED],       _("Core Speed"));
-	asprintf(&data->tab_cpu[NAME][MULTIPLIER],      _("Multiplier"));
-	asprintf(&data->tab_cpu[NAME][BUSSPEED],        _("Bus Speed"));
-	asprintf(&data->tab_cpu[NAME][USAGE],           _("Usage"));
-
-	asprintf(&data->objects[FRAMCACHE],             _("Cache")); // Frame label
-	asprintf(&data->tab_cpu[NAME][LEVEL1D],         _("L1 Data"));
-	asprintf(&data->tab_cpu[NAME][LEVEL1I],         _("L1 Inst."));
-	asprintf(&data->tab_cpu[NAME][LEVEL2],          _("Level 2"));
-	asprintf(&data->tab_cpu[NAME][LEVEL3],          _("Level 3"));
-
-	asprintf(&data->tab_cpu[NAME][SOCKETS],         _("Socket(s)"));
-	asprintf(&data->tab_cpu[NAME][CORES],           _("Core(s)"));
-	asprintf(&data->tab_cpu[NAME][THREADS],         _("Thread(s)"));
-
-	/* Caches tab */
-	asprintf(&data->objects[TABCACHES], _("Caches")); // Tab label
-	for(i = L1SIZE; i < LASTCACHES; i += CACHEFIELDS)
+	Arrays arrays[] =
 	{
-		j = i / CACHEFIELDS;
-		asprintf(&data->objects[FRAML1CACHE + j],      _("L%i Cache"), j + 1); // Frame label
-		asprintf(&data->tab_caches[NAME][L1SIZE  + i], _("Size"));
-		asprintf(&data->tab_caches[NAME][L1SPEED + i], _("Speed"));
-	}
-	asprintf(&data->objects[FRAMTEST], _("Test"));
-
-	/* Motherboard tab */
-	asprintf(&data->objects[TABMOTHERBOARD],              _("Motherboard")); // Tab label
-	asprintf(&data->objects[FRAMMOTHERBOARD],             _("Motherboard")); // Frame label
-	asprintf(&data->tab_motherboard[NAME][MANUFACTURER],  _("Manufacturer"));
-	asprintf(&data->tab_motherboard[NAME][MBMODEL],       _("Model"));
-	asprintf(&data->tab_motherboard[NAME][REVISION],      _("Revision"));
-
-	asprintf(&data->objects[FRAMBIOS],                    _("BIOS")); // Frame label
-	asprintf(&data->tab_motherboard[NAME][BRAND],         _("Brand"));
-	asprintf(&data->tab_motherboard[NAME][BIOSVERSION],   _("Version"));
-	asprintf(&data->tab_motherboard[NAME][DATE],          _("Date"));
-	asprintf(&data->tab_motherboard[NAME][ROMSIZE],       _("ROM Size"));
-
-	asprintf(&data->objects[FRAMCHIPSET],                 _("Chipset")); // Frame label
-	asprintf(&data->tab_motherboard[NAME][CHIPVENDOR],    _("Vendor"));
-	asprintf(&data->tab_motherboard[NAME][CHIPMODEL],     _("Model"));
-
-	/* Memory tab */
-	asprintf(&data->objects[TABMEMORY], _("Memory")); // Tab label
-	for(i = BANK0; i < LASTMEMORY; i++)
-	{
-		asprintf(&data->objects[FRAMBANK0 + i], _("Bank %i"), i);
-		asprintf(&data->tab_memory[NAME][i],    _("Reference"));
-	}
-
-	/* System tab */
-	asprintf(&data->objects[TABSYSTEM],                   _("System")); // Tab label
-	asprintf(&data->objects[FRAMOPERATINGSYSTEM],         _("Operating System")); // Frame label
-	asprintf(&data->tab_system[NAME][KERNEL],             _("Kernel"));
-	asprintf(&data->tab_system[NAME][DISTRIBUTION],       _("Distribution"));
-	asprintf(&data->tab_system[NAME][HOSTNAME],           _("Hostname"));
-	asprintf(&data->tab_system[NAME][UPTIME],             _("Uptime"));
-	asprintf(&data->tab_system[NAME][COMPILER],           _("Compiler"));
-
-	asprintf(&data->objects[FRAMMEMORY],                  _("Memory")); // Frame label
-	asprintf(&data->tab_system[NAME][USED],               _("Used"));
-	asprintf(&data->tab_system[NAME][BUFFERS],            _("Buffers"));
-	asprintf(&data->tab_system[NAME][CACHED],             _("Cached"));
-	asprintf(&data->tab_system[NAME][FREE],               _("Free"));
-	asprintf(&data->tab_system[NAME][SWAP],               _("Swap"));
-
-	/* Graphics tab */
-	asprintf(&data->objects[TABGRAPHICS], _("Graphics")); // Tab label
-	for(i = GPU1VENDOR; i < LASTGRAPHICS; i += GPUFIELDS)
-	{
-		j = i / GPUFIELDS;
-		asprintf(&data->objects[FRAMGPU1 + j],                   _("Card %i"), j); // Frame label
-		asprintf(&data->tab_graphics[NAME][GPU1VENDOR      + i], _("Vendor"));
-		asprintf(&data->tab_graphics[NAME][GPU1MODEL       + i], _("Model"));
-		asprintf(&data->tab_graphics[NAME][GPU1TEMPERATURE + i], _("Temperature"));
-	}
-
-	/* Bench tab */
-	asprintf(&data->objects[TABBENCH],              _("Bench")); // Tab label
-	asprintf(&data->objects[FRAMPRIMESLOW],         _("Prime numbers (slow)")); // Frame label
-	asprintf(&data->objects[FRAMPRIMEFAST],         _("Prime numbers (fast)")); // Frame label
-	for(i = PRIMESLOWSCORE; i < PARAMDURATION; i += BENCHFIELDS)
-	{
-		asprintf(&data->tab_bench[NAME][PRIMESLOWSCORE + i], _("Score"));
-		asprintf(&data->tab_bench[NAME][PRIMESLOWRUN   + i], _("Run"));
-	}
-
-	asprintf(&data->objects[FRAMPARAM],             _("Parameters")); // Frame label
-	asprintf(&data->tab_bench[NAME][PARAMDURATION], _("Duration"));
-	asprintf(&data->tab_bench[NAME][PARAMTHREADS],  _("Threads"));
-
-	/* About tab */
-	asprintf(&data->objects[TABABOUT],              _("About")); // Tab label
-	asprintf(&data->tab_about[DESCRIPTION],         _(
-		"%s is a Free software that gathers information\n"
-		"on CPU, motherboard and more."), PRGNAME);
-
-	asprintf(&data->objects[FRAMABOUT],             _("About")); // Frame label
-	asprintf(&data->tab_about[VERSIONSTR],          _("Version %s"), PRGVER);
-	asprintf(&data->tab_about[AUTHOR],              _("Author: %s"), PRGAUTH);
-	asprintf(&data->tab_about[SITE],                _("Site: %s"),   PRGURL);
-
-	asprintf(&data->objects[FRAMLICENSE],           _("License")); // Frame label
-	asprintf(&data->tab_about[COPYRIGHT],           PRGCPYR);
-	asprintf(&data->tab_about[LICENSE],             _(
-		"This software is distributed under the terms of GNU GPL v3"));
-	asprintf(&data->tab_about[NOWARRANTY],            _(
-		"This program comes with ABSOLUTELY NO WARRANTY"));
-}
-
-/* Replace null pointers by character '\0' */
-static int remove_null_ptr(Labels *data)
-{
-	int i, j, ret = 0;
-	const struct Arrays { char **array; const int last; } a[] =
-	{
-		{ data->tab_cpu[VALUE],         LASTCPU         },
-		{ data->tab_caches[VALUE],      LASTCACHES      },
-		{ data->tab_motherboard[VALUE], LASTMOTHERBOARD },
-		{ data->tab_memory[VALUE],      LASTMEMORY      },
-		{ data->tab_system[VALUE],      LASTSYSTEM      },
-		{ data->tab_graphics[VALUE],    LASTGRAPHICS    },
-		{ data->tab_bench[VALUE],       LASTBENCH       },
-		{ NULL,                         0               }
+		{ data->objects,               NULL,                         objects_names,         LASTOBJ         },
+		{ data->tab_cpu[NAME],         data->tab_cpu[VALUE],         tab_cpu_names,         LASTCPU         },
+		{ data->tab_caches[NAME],      data->tab_caches[VALUE],      tab_caches_names,      LASTCACHES      },
+		{ data->tab_motherboard[NAME], data->tab_motherboard[VALUE], tab_motherboard_names, LASTMOTHERBOARD },
+		{ data->tab_memory[NAME],      data->tab_memory[VALUE],      tab_memory_names,      LASTMEMORY      },
+		{ data->tab_system[NAME],      data->tab_system[VALUE],      tab_system_names,      LASTSYSTEM      },
+		{ data->tab_graphics[NAME],    data->tab_graphics[VALUE],    tab_graphics_names,    LASTGRAPHICS    },
+		{ data->tab_bench[NAME],       data->tab_bench[VALUE],       tab_bench_names,       LASTBENCH       },
+		{ data->tab_about,             NULL,                         tab_about_names,       LASTABOUT       },
+		{ NULL,                        NULL,                         NULL,                  0               }
 	};
 
-	MSG_VERBOSE(_("Replacing undefined labels by an empty string"));
-	for(i = 0; a[i].array != NULL; i++)
+	MSG_VERBOSE(_("Setting label names"));
+	for(int a = 0; arrays[a].dim_names != NULL; a++)
 	{
-		for(j = 0; j < a[i].last; j++)
+		for(int n = 0; n < arrays[a].last; n++)
 		{
-			if(a[i].array[j] == NULL)
-				ret += casprintf(&a[i].array[j], false, "%c", '\0');
+			asprintf(&arrays[a].dim_names[arrays[a].names[n].index], _(arrays[a].names[n].name));
+			if(arrays[a].dim_values != NULL)
+				casprintf(&arrays[a].dim_values[n], false, "%c", '\0');
 		}
 	}
-
-	return ret;
 }
 
 /* Dump all data in stdout */
@@ -547,7 +412,6 @@ static int update_prg(void)
 
 /************************* Options-related functions *************************/
 
-#define N_(x) x
 static const struct
 {
 	const bool has_mod;
@@ -586,7 +450,6 @@ static const struct
 	{ HAS_LIBCPUID, "CPUX_CPUID_RAW", N_("Read CPUID raw data from a given file") },
 	{ true,         NULL,             NULL                                        }
 };
-#undef N_
 
 /* This is help display with --help option */
 static void help(void)
@@ -633,10 +496,10 @@ static void version(void)
 		check_new_version();
 
 	MSG_STDOUT("%s %s %s", PRGNAME, PRGVER, new_version[1]);
-	MSG_STDOUT("%s\n", PRGCPYR);
+	MSG_STDOUT("%s\n", PRGCPRGHT);
 	MSG_STDOUT(_("This is free software: you are free to change and redistribute it."));
 	MSG_STDOUT(_("This program comes with ABSOLUTELY NO WARRANTY"));
-	MSG_STDOUT(_("See the GPLv3 license: <http://www.gnu.org/licenses/gpl.txt>\n"));
+	MSG_STDOUT(_("See the %s license: <%s>\n"), PRGLCNS, LCNSURL);
 	MSG_STDOUT(_("Built on %s, %s (with %s %s on %s)."), __DATE__, __TIME__, CC, __VERSION__, OS);
 	free(new_version[1]);
 
@@ -913,7 +776,6 @@ int main(int argc, char *argv[])
 		}
 		labels_setname (data);
 		fill_labels    (data);
-		remove_null_ptr(data);
 
 		if(HAS_LIBCURL)
 			check_new_version();

commit 452dfd29ce405ecd15f629e700b24afa2bfbefff

Fix regressions introduced in 5f78a7f

src/core.c
@@ -615,6 +615,7 @@ static int call_dmidecode(Labels *data)
 	/* Tab RAM */
 	for(i = BANK0; i < LASTMEMORY; i++)
 		dmidata[DMI_RAM][i] = &data->tab_memory[VALUE][i];
+	bank = (unsigned*) &data->dimm_count;
 	opt.type[17] = 1;
 
 	/* Call built-in dmidecode in CPU-X mode */
@@ -622,9 +623,6 @@ static int call_dmidecode(Labels *data)
 
 	if(data->tab_cpu[VALUE][BUSSPEED] != NULL)
 		data->bus_freq = strtod(data->tab_cpu[VALUE][BUSSPEED], NULL);
-
-	//while(data->tab_memory[VALUE][data->dimm_count] != NULL) //FIXME
-	//	data->dimm_count++;
 
 	if(err)
 		MSG_ERROR(_("failed to call dmidecode"));
@@ -1153,8 +1151,7 @@ static int cputab_package_fallback(Labels *data)
 	{
 		MSG_WARNING(_("Your CPU socket does not belong in database ==> %s, codename: %s"),
 		            data->tab_cpu[VALUE][SPECIFICATION], data->tab_cpu[VALUE][CODENAME]);
-		free(data->tab_cpu[VALUE][PACKAGE]);
-		data->tab_cpu[VALUE][PACKAGE] = NULL;
+		data->tab_cpu[VALUE][PACKAGE][0] = '\0';
 		return 2;
 	}
 }

src/dmidecode/dmidecode.c
@@ -81,6 +81,7 @@ static const char *bad_index = "<BAD INDEX>";
 
 /* Options are global */
 struct opt opt;
+unsigned *bank;
 char **dmidata[LASTDMI][16];
 
 /*
@@ -3274,7 +3275,6 @@ static const char *dmi_management_controller_host_type(u8 code)
 static void dmi_decode(const struct dmi_header *h, u16 ver)
 {
 	const u8 *data = h->data;
-	static int bank = BANK0;
 
 	/*
 	 * Note: DMI types 37 and 42 are untested
@@ -3754,13 +3754,13 @@ static void dmi_decode(const struct dmi_header *h, u16 ver)
 			break;
 
 		case 17: /* 7.18 Memory Device */
-			if((opt.flags & FLAG_CPU_X) && bank < LASTMEMORY)
+			if((opt.flags & FLAG_CPU_X) && *bank < LASTMEMORY)
 			{
 				if(strstr(dmi_string(h, data[0x17]), "Empty") != NULL)
-					casprintf(dmidata[DMI_RAM][bank],   false, "- - - - - - - - - - - - - - - - - - -");
+					casprintf(dmidata[DMI_RAM][*bank], false, "- - - - - - - - - - - - - - - - - - -");
 				else
 				{
-					casprintf(dmidata[DMI_RAM][bank], true, "%s %s, %s @ %uMHz (%s %s)",
+					casprintf(dmidata[DMI_RAM][*bank], true, "%s %s, %s @ %uMHz (%s %s)",
 					          dmi_string(h, data[0x17]),
 					          dmi_string(h, data[0x1A]),
 						  dmi_memory_device_size_str(WORD(data + 0x0C)),
@@ -3768,7 +3768,7 @@ static void dmi_decode(const struct dmi_header *h, u16 ver)
 					          dmi_memory_device_form_factor(data[0x0E]),
 					          dmi_memory_device_type(data[0x12]));
 				}
-				bank++;
+				(*bank)++;
 			}
 			else
 			{

src/dmidecode/libdmi.h
@@ -35,6 +35,7 @@ enum EnDmidecode
 	DMI_CPU, DMI_MB, DMI_RAM, LASTDMI
 };
 
+extern unsigned *bank;
 extern char **dmidata[LASTDMI][16];
 
 int dmidecode(void);