86dd13e18925eb49656f1acde33055e3d5f253b9
Xorg <xorgbreaker@gmail.com>
2017-02-05 17:39:46 +0100

    Fix Cache labels format Related to #47

--- src/core.c
+++ src/core.c
@@ -199,8 +199,9 @@ static int call_libcpuid_static(Labels *data)
 {
 	int err, i, j = 0;
 	char tmp[MAXSTR] = "";
-	const char *fmt_kb = { _("%d x %d KB, %d-way associative, %d-byte line size") };
-	const char *fmt_mb = { _("%d MB, %d-way associative, %d-byte line size") };
+	const char *fmt_cache_kb = _("%d x %d KB, %d-way");
+	const char *fmt_cache_mb = _("%d MB, %d-way");
+	const char *fmt_lines    = _("%s associative, %d-byte line size");
 	struct cpu_raw_data_t raw;
 	struct cpu_id_t datanr;
 
@@ -268,16 +269,15 @@ static int call_libcpuid_static(Labels *data)
 
 	/* Cache level 1 (data) */
 	if(datanr.l1_data_cache > 0)
-		casprintf(&data->tab_cpu[VALUE][LEVEL1D], true, "%d x %4d KB, %2d-way", datanr.num_cores, datanr.l1_data_cache, datanr.l1_assoc);
+		casprintf(&data->tab_cpu[VALUE][LEVEL1D], true, fmt_cache_kb, datanr.num_cores, datanr.l1_data_cache, datanr.l1_assoc);
 
 	/* Cache level 1 (instruction) */
 	if(datanr.l1_instruction_cache > 0)
 	{
 		data->cache_count++;
 		data->w_data->size[0] = datanr.l1_instruction_cache;
-		casprintf(&data->tab_cpu[VALUE][LEVEL1I], true, "%d x %4d KB, %2d-way", datanr.num_cores, datanr.l1_instruction_cache, datanr.l1_assoc);
-		casprintf(&data->tab_caches[VALUE][L1SIZE], true, fmt_kb, datanr.num_cores, datanr.l1_instruction_cache,
-			datanr.l1_assoc, datanr.l1_cacheline);
+		casprintf(&data->tab_cpu[VALUE][LEVEL1I], true, fmt_cache_kb, datanr.num_cores, datanr.l1_instruction_cache, datanr.l1_assoc);
+		casprintf(&data->tab_caches[VALUE][L1SIZE], true, fmt_lines, data->tab_cpu[VALUE][LEVEL1I], datanr.l1_cacheline);
 	}
 
 	/* Cache level 2 */
@@ -285,9 +285,8 @@ static int call_libcpuid_static(Labels *data)
 	{
 		data->cache_count++;
 		data->w_data->size[1] = datanr.l2_cache;
-		casprintf(&data->tab_cpu[VALUE][LEVEL2], true, "%d x %4d KB, %2d-way", datanr.num_cores, datanr.l2_cache, datanr.l2_assoc);
-		casprintf(&data->tab_caches[VALUE][L2SIZE], true, fmt_kb, datanr.num_cores, datanr.l2_cache,
-			datanr.l2_assoc, datanr.l2_cacheline);
+		casprintf(&data->tab_cpu[VALUE][LEVEL2], true, fmt_cache_kb, datanr.num_cores, datanr.l2_cache, datanr.l2_assoc);
+		casprintf(&data->tab_caches[VALUE][L2SIZE], true, fmt_lines, data->tab_cpu[VALUE][LEVEL2], datanr.l2_cacheline);
 	}
 
 	/* Cache level 3 */
@@ -295,9 +294,8 @@ static int call_libcpuid_static(Labels *data)
 	{
 		data->cache_count++;
 		data->w_data->size[2] = datanr.l3_cache;
-		casprintf(&data->tab_cpu[VALUE][LEVEL3], true, "%4d MB, %2d-way", datanr.l3_cache / (1 << 10), datanr.l3_assoc);
-		casprintf(&data->tab_caches[VALUE][L3SIZE], true, fmt_mb, datanr.l3_cache  / (1 << 10),
-			datanr.l3_assoc, datanr.l3_cacheline);
+		casprintf(&data->tab_cpu[VALUE][LEVEL3], true, fmt_cache_mb, datanr.l3_cache >> 10, datanr.l3_assoc);
+		casprintf(&data->tab_caches[VALUE][L3SIZE], true, fmt_lines, data->tab_cpu[VALUE][LEVEL3], datanr.l3_cacheline);
 	}
 
 	/* Cache level 4 */
@@ -305,8 +303,9 @@ static int call_libcpuid_static(Labels *data)
 	{
 		data->cache_count++;
 		data->w_data->size[3] = datanr.l4_cache;
-		casprintf(&data->tab_caches[VALUE][L4SIZE], true, fmt_mb, datanr.l4_cache  / (1 << 10),
-			datanr.l4_assoc, datanr.l4_cacheline);
+		snprintf(tmp, MAXSTR, fmt_cache_mb, datanr.l4_cache >> 10, datanr.l4_assoc);
+		casprintf(&data->tab_caches[VALUE][L4SIZE], true, fmt_lines, tmp, datanr.l4_cacheline);
+		memset(tmp, 0, MAXSTR);
 	}
 
 	if(datanr.total_logical_cpus < datanr.num_logical_cpus)
