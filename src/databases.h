01cc090042b20040e7a75484d73423d4826911c5
Xorg <xorgbreaker@gmail.com>
2018-04-24 13:11:17 +0200

    DB: Add Pinnacle Ridge CPUs

--- src/databases.h
+++ src/databases.h
@@ -101,7 +101,7 @@ const Technology_DB technology_amd[] =
 	{  1,          -1,          20,          40 }, // Bobcat (Brazos Zacate)
 	{  1,           1,          21,          32 }, // Bulldozer
 	{  1,          96,          21,          28 }, // Bulldozer (Excavator: Carrizo)
-	{  1,          -1,          23,          14 }, // Zen (Summit Ridge)
+	{  1,          -1,          23,          14 }, // Zen (Summit Ridge/Raven Ridge)
 	{  2,          -1,          16,          65 }, // K10 (Kuma/Agena)
 	{  2,          -1,          20,          40 }, // Bobcat (Brazos Desna/Ontario)
 	{  2,          -1,          21,          32 }, // Bulldozer (Piledriver: Vishera)
@@ -115,6 +115,7 @@ const Technology_DB technology_amd[] =
 	{  8,          -1,           6,         130 }, // K7 (Thoroughbred/Applebred)
 	{  8,          -1,          15,          65 }, // K8 (Tyler)
 	{  8,          -1,          21,          28 }, // Bulldozer (Steamroller: Godavari)
+	{  8,          -1,          23,          12 }, // Zen+ (Pinnacle Ridge)
 	{  9,          -1,          16,          45 }, // Magny-Cours (Opteron)
 	{ 10,          -1,           6,         130 }, // K7 (Barton)
 	{ 10,          -1,          16,          45 }, // K10 (Thuban)
