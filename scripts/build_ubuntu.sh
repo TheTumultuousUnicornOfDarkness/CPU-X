75e264308f29ff4375ed47f2647df01a91b24e7f
Xorg <xorgbreaker@gmail.com>
2020-05-16 20:28:18 +0200

    CI: fix libcpuid install There are troubles ATM with OBS

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -9,13 +9,14 @@ BUILD_TYPE="$1"
 SRC_DIR="$2"
 DST_DIR="$3"
 
-echo "Add OBS repository"
-curl http://download.opensuse.org/repositories/home:/X0rg/xUbuntu_16.04/Release.key | sudo apt-key add -
-echo "deb http://download.opensuse.org/repositories/home:/X0rg/xUbuntu_16.04/ /" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update -qq
+# echo "Add OBS repository"
+# curl http://download.opensuse.org/repositories/home:/X0rg/xUbuntu_16.04/Release.key | sudo apt-key add -
+# echo "deb http://download.opensuse.org/repositories/home:/X0rg/xUbuntu_16.04/ /" | sudo tee -a /etc/apt/sources.list
+# sudo apt-get update -qq
 
 echo "Install packages"
-sudo apt-get install -y --allow-unauthenticated -qq cmake ninja-build nasm gettext libgtk-3-dev libncursesw5-dev libcpuid-dev libpci-dev libprocps-dev libarchive-dev libgtk-3-0 libncursesw5 libcpuid14 libpci3 libprocps4 adwaita-icon-theme
+sudo dpkg -i $GITHUB_WORKSPACE/.github/blobs/*.deb
+sudo apt-get install -y -qq cmake ninja-build nasm gettext libgtk-3-dev libncursesw5-dev libpci-dev libprocps-dev libgtk-3-0 libncursesw5 libpci3 libprocps4 adwaita-icon-theme
 
 echo "Run CMake"
 cmake -S "$SRC_DIR" -B build -GNinja -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBEXECDIR=/usr/bin
