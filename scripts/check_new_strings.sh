3bdeb1855f0bb393aa3fcd0cf3544bb12d9ff593
Xorg <xorgbreaker@gmail.com>
2021-03-27 14:43:47 +0100

    Scripts: fix check_new_strings

--- scripts/check_new_strings.sh
+++ scripts/check_new_strings.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# This script is used to create archives for releases
+# This script is used to check if there are new strings in source files
 
 set -euo pipefail
 
 GIT_DIR="$(git rev-parse --show-toplevel)"
+cd "$GIT_DIR" || exit 1
 
-if [[ "$(git show -s --format='%ce' HEAD~0)" == "hosted@weblate.org" ]]; then
+if [[ "$(git show -s --format='%ce')" == "hosted@weblate.org" ]]; then
 	echo "Last commit by WebLate, skipping commit."
 	exit 1
 fi
 
-for file in $(git diff --name-only HEAD~0 HEAD~1); do
+for file in $(git show --pretty="" --name-only); do
 	if [[ "$file" == *.c ]] || [[ "$file" == *.h ]]; then
 		if git show "$file" | egrep '^(\+|\-)' | egrep -qw '_\(|N_\('; then
 			echo "Found a string change in $file."
