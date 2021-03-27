d82ed0dfb91491c50dd452bbd7f4d77ba786d555
Xorg <xorgbreaker@gmail.com>
2021-03-27 14:50:28 +0100

    Scripts: add commit SHA in check_new_strings

--- scripts/check_new_strings.sh
+++ scripts/check_new_strings.sh
@@ -4,16 +4,17 @@
 set -euo pipefail
 
 GIT_DIR="$(git rev-parse --show-toplevel)"
+SHA=${GITHUB_SHA:-HEAD~0}
 cd "$GIT_DIR" || exit 1
 
-if [[ "$(git show -s --format='%ce')" == "hosted@weblate.org" ]]; then
+if [[ "$(git show -s --format='%ce' "$SHA")" == "hosted@weblate.org" ]]; then
 	echo "Last commit by WebLate, skipping commit."
 	exit 1
 fi
 
-for file in $(git show --pretty="" --name-only); do
+for file in $(git show --pretty="" --name-only "$SHA"); do
 	if [[ "$file" == *.c ]] || [[ "$file" == *.h ]]; then
-		if git show "$file" | egrep '^(\+|\-)' | egrep -qw '_\(|N_\('; then
+		if git show "$SHA" "$file" | egrep '^(\+|\-)' | egrep -qw '_\(|N_\('; then
 			echo "Found a string change in $file."
 			exit 0
 		fi
