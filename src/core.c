93807958bf74497e88b069116c6e01e6c9d520fb
Xorg <xorgbreaker@gmail.com>
2022-08-22 19:15:58 +0200

    Core: print Vulkan errors

--- src/core.c
+++ src/core.c
@@ -850,6 +850,119 @@ clean:
 	return err;
 }
 
+#if HAS_Vulkan
+static inline const char* string_VkResult(VkResult input_value)
+{
+	switch (input_value)
+	{
+		case VK_ERROR_COMPRESSION_EXHAUSTED_EXT:
+			return "VK_ERROR_COMPRESSION_EXHAUSTED_EXT";
+		case VK_ERROR_DEVICE_LOST:
+			return "VK_ERROR_DEVICE_LOST";
+		case VK_ERROR_EXTENSION_NOT_PRESENT:
+			return "VK_ERROR_EXTENSION_NOT_PRESENT";
+		case VK_ERROR_FEATURE_NOT_PRESENT:
+			return "VK_ERROR_FEATURE_NOT_PRESENT";
+		case VK_ERROR_FORMAT_NOT_SUPPORTED:
+			return "VK_ERROR_FORMAT_NOT_SUPPORTED";
+		case VK_ERROR_FRAGMENTATION:
+			return "VK_ERROR_FRAGMENTATION";
+		case VK_ERROR_FRAGMENTED_POOL:
+			return "VK_ERROR_FRAGMENTED_POOL";
+		case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT:
+			return "VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT";
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR:
+			return "VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR";
+#endif // VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
+			return "VK_ERROR_INCOMPATIBLE_DISPLAY_KHR";
+		case VK_ERROR_INCOMPATIBLE_DRIVER:
+			return "VK_ERROR_INCOMPATIBLE_DRIVER";
+		case VK_ERROR_INITIALIZATION_FAILED:
+			return "VK_ERROR_INITIALIZATION_FAILED";
+		case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT:
+			return "VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT";
+		case VK_ERROR_INVALID_EXTERNAL_HANDLE:
+			return "VK_ERROR_INVALID_EXTERNAL_HANDLE";
+		case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS:
+			return "VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS";
+		case VK_ERROR_INVALID_SHADER_NV:
+			return "VK_ERROR_INVALID_SHADER_NV";
+		case VK_ERROR_LAYER_NOT_PRESENT:
+			return "VK_ERROR_LAYER_NOT_PRESENT";
+		case VK_ERROR_MEMORY_MAP_FAILED:
+			return "VK_ERROR_MEMORY_MAP_FAILED";
+		case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
+			return "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
+		case VK_ERROR_NOT_PERMITTED_KHR:
+			return "VK_ERROR_NOT_PERMITTED_KHR";
+		case VK_ERROR_OUT_OF_DATE_KHR:
+			return "VK_ERROR_OUT_OF_DATE_KHR";
+		case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+			return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
+		case VK_ERROR_OUT_OF_HOST_MEMORY:
+			return "VK_ERROR_OUT_OF_HOST_MEMORY";
+		case VK_ERROR_OUT_OF_POOL_MEMORY:
+			return "VK_ERROR_OUT_OF_POOL_MEMORY";
+		case VK_ERROR_SURFACE_LOST_KHR:
+			return "VK_ERROR_SURFACE_LOST_KHR";
+		case VK_ERROR_TOO_MANY_OBJECTS:
+			return "VK_ERROR_TOO_MANY_OBJECTS";
+		case VK_ERROR_UNKNOWN:
+			return "VK_ERROR_UNKNOWN";
+		case VK_ERROR_VALIDATION_FAILED_EXT:
+			return "VK_ERROR_VALIDATION_FAILED_EXT";
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR:
+			return "VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR";
+#endif // VK_ENABLE_BETA_EXTENSIONS
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR:
+			return "VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR";
+#endif // VK_ENABLE_BETA_EXTENSIONS
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR:
+			return "VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR";
+#endif // VK_ENABLE_BETA_EXTENSIONS
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR:
+			return "VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR";
+#endif // VK_ENABLE_BETA_EXTENSIONS
+#ifdef VK_ENABLE_BETA_EXTENSIONS
+		case VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR:
+			return "VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR";
+#endif // VK_ENABLE_BETA_EXTENSIONS
+		case VK_EVENT_RESET:
+			return "VK_EVENT_RESET";
+		case VK_EVENT_SET:
+			return "VK_EVENT_SET";
+		case VK_INCOMPLETE:
+			return "VK_INCOMPLETE";
+		case VK_NOT_READY:
+			return "VK_NOT_READY";
+		case VK_OPERATION_DEFERRED_KHR:
+			return "VK_OPERATION_DEFERRED_KHR";
+		case VK_OPERATION_NOT_DEFERRED_KHR:
+			return "VK_OPERATION_NOT_DEFERRED_KHR";
+		case VK_PIPELINE_COMPILE_REQUIRED:
+			return "VK_PIPELINE_COMPILE_REQUIRED";
+		case VK_SUBOPTIMAL_KHR:
+			return "VK_SUBOPTIMAL_KHR";
+		case VK_SUCCESS:
+			return "VK_SUCCESS";
+		case VK_THREAD_DONE_KHR:
+			return "VK_THREAD_DONE_KHR";
+		case VK_THREAD_IDLE_KHR:
+			return "VK_THREAD_IDLE_KHR";
+		case VK_TIMEOUT:
+			return "VK_TIMEOUT";
+		default:
+			return "Unhandled VkResult";
+	}
+}
+#endif /* HAS_Vulkan */
+
 static int get_vulkan_api_version(uint32_t device_id, char *vulkan_version, bool *vulkan_rt)
 {
 	int err = 0;
@@ -864,18 +977,18 @@ static int get_vulkan_api_version(uint32_t device_id, char *vulkan_version, bool
 	VkInstance instance = {0};
 	VkInstanceCreateInfo createInfo = {0};
 
-	if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS)
+	if ((err = vkCreateInstance(&createInfo, NULL, &instance)) != VK_SUCCESS)
 	{
-		MSG_ERROR("%s", _("failed to call vkCreateInstance"));
+		MSG_ERROR(_("failed to call vkCreateInstance (%s)"), string_VkResult(err));
 		return err;
 	}
 
 	VkPhysicalDevice *devices = NULL;
 
 	/* get number of devices */
-	if (vkEnumeratePhysicalDevices(instance, &device_count, NULL) != VK_SUCCESS)
+	if ((err = vkEnumeratePhysicalDevices(instance, &device_count, NULL)) != VK_SUCCESS)
 	{
-		MSG_ERROR("%s", _("failed to call vkEnumeratePhysicalDevices"));
+		MSG_ERROR(_("failed to call vkEnumeratePhysicalDevices (%s)"), string_VkResult(err));
 		return err;
 	}
 
@@ -888,9 +1001,9 @@ static int get_vulkan_api_version(uint32_t device_id, char *vulkan_version, bool
 	devices = malloc(sizeof(VkPhysicalDevice) * device_count);
 
 	/* get all device handles */
-	if (vkEnumeratePhysicalDevices(instance, &device_count, devices) != VK_SUCCESS)
+	if ((err = vkEnumeratePhysicalDevices(instance, &device_count, devices)) != VK_SUCCESS)
 	{
-		MSG_WARNING("%s", _("There is no available physical device"));
+		MSG_WARNING(_("There is no available physical device (%s)"), string_VkResult(err));
 		free(devices);
 		return err;
 	}
