d0c12782933f2ec28376c7c78f21f81f627cd5e0
The Tumultuous Unicorn Of Darkness <the-tumultuous-unicorn-of-darkness@gmx.com>
2023-04-04 19:33:20 +0200

    Core: fix cputab_temp_fallback() on FreeBSD
    Patch from https://github.com/TheTumultuousUnicornOfDarkness/CPU-X/issues/121#issuecomment-1495299295

--- src/core.c
+++ src/core.c
@@ -2250,11 +2250,11 @@ static int cputab_temp_fallback(Labels *data)
 	}
 #else /* __linux__ */
 	/* Tested on FreeBSD 12: https://github.com/TheTumultuousUnicornOfDarkness/CPU-X/issues/121#issuecomment-575985765 */
-	char temp[MAXSTR];
-	size_t len = sizeof(temp);
+	int dK;
+	size_t len = sizeof(dK);
 
-	if(!(err = sysctlbyname(format("dev.cpu.%i.temperature", data->current_core_id), temp, &len, NULL, 0)))
-		casprintf(&data->tab_cpu[VALUE][TEMPERATURE], true, "%s", temp);
+	if(!(err = sysctlbyname(format("dev.cpu.%i.temperature", data->current_core_id), &dK, &len, NULL, 0)))
+		casprintf(&data->tab_cpu[VALUE][TEMPERATURE], true, "%.1f°C", (dK - 2731) / 10.);
 #endif /* __linux__ */
 
 	if(err)
