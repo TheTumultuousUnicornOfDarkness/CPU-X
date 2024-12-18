515d154ffdb963f7659184f3ce4c768ea539e64f
The Tumultuous Unicorn Of Darkness <the-tumultuous-unicorn-of-darkness@gmx.com>
2024-12-18 16:30:19 +0100

    [GUI] Fix AppImage build

--- src/gui_gtk.cpp
+++ src/gui_gtk.cpp
@@ -67,9 +67,11 @@ static inline bool check_data_path(const std::string file, const std::string ful
 /* Search file location in standard paths */
 static const std::string get_data_path(const std::string &file)
 {
+	std::string full_path;
+
 #if not defined (APPIMAGE) && not defined (FLATPAK)
 	/* Try to open file under CPU_X_DATA_DIRECTORY (CMake variable) */
-	std::string full_path = std::string(CPUX_DATA_DIRECTORY) + "/" + file;
+	full_path = std::string(CPUX_DATA_DIRECTORY) + "/" + file;
 	if(check_data_path(file, full_path))
 		return full_path;
 #endif
